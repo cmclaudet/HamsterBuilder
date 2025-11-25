a7a4bc07935c3c66b7d1872b03a74cbf7302d0e5
Catherine Claudet <cmclaudet@gmail.com>
2025-11-25 22:05:25 +0100

    Add entry point data to wheel

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -1,6 +1,12 @@
+using System.Linq;
 using UnityEngine;
 
 public class Wheel : PlaceableObject
 {
+    public GameObject[] Entries;
 
+    public override Vector3[] GetEntryPoints()
+    {
+        return Entries.Select(e => e.transform.position).ToArray();
+    }
 }
