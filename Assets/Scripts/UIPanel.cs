2612e70507351a95bd09445995d614aa09e986c2
Catherine Claudet <cmclaudet@gmail.com>
2025-11-25 11:52:00 +0100

    Set up scene with UI, add TMP

---
+++ Assets/Scripts/UIPanel.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanel : MonoBehaviour
+{
+    public Button ButtonPrefab;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
